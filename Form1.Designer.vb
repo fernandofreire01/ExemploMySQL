72ad7246c0dd0d8fcc2f2bef13e452f6597eb170
Fernando Freire <fernando.freire.94@hotmail.com>
2020-06-24 14:21:59 -0300

    Finalizado exemplo 2 (funcionou normalmente)

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,30 +22,124 @@ Partial Class Form1
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.lblFoto = New System.Windows.Forms.Label()
+        Me.picFoto = New System.Windows.Forms.PictureBox()
+        Me.btnLocalizar = New System.Windows.Forms.Button()
+        Me.txtNome = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnSalvar = New System.Windows.Forms.Button()
+        Me.btnEncerrar = New System.Windows.Forms.Button()
+        CType(Me.picFoto, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(29, 29)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(32, 13)
+        Me.lblEmail.TabIndex = 0
+        Me.lblEmail.Text = "Email"
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(32, 45)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(301, 20)
+        Me.txtEmail.TabIndex = 1
+        '
+        'lblFoto
+        '
+        Me.lblFoto.AutoSize = True
+        Me.lblFoto.Location = New System.Drawing.Point(29, 82)
+        Me.lblFoto.Name = "lblFoto"
+        Me.lblFoto.Size = New System.Drawing.Size(28, 13)
+        Me.lblFoto.TabIndex = 2
+        Me.lblFoto.Text = "Foto"
+        '
+        'picFoto
+        '
+        Me.picFoto.Location = New System.Drawing.Point(32, 99)
+        Me.picFoto.Name = "picFoto"
+        Me.picFoto.Size = New System.Drawing.Size(200, 200)
+        Me.picFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picFoto.TabIndex = 3
+        Me.picFoto.TabStop = False
+        '
+        'btnLocalizar
+        '
+        Me.btnLocalizar.Location = New System.Drawing.Point(238, 99)
+        Me.btnLocalizar.Name = "btnLocalizar"
+        Me.btnLocalizar.Size = New System.Drawing.Size(57, 23)
+        Me.btnLocalizar.TabIndex = 4
+        Me.btnLocalizar.Text = "Localizar"
+        Me.btnLocalizar.UseVisualStyleBackColor = True
+        '
+        'txtNome
+        '
+        Me.txtNome.Location = New System.Drawing.Point(32, 329)
+        Me.txtNome.Name = "txtNome"
+        Me.txtNome.Size = New System.Drawing.Size(301, 20)
+        Me.txtNome.TabIndex = 6
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(75, 47)
+        Me.Label1.Location = New System.Drawing.Point(29, 313)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(32, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Email"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Nome"
+        '
+        'btnSalvar
+        '
+        Me.btnSalvar.Location = New System.Drawing.Point(32, 375)
+        Me.btnSalvar.Name = "btnSalvar"
+        Me.btnSalvar.Size = New System.Drawing.Size(96, 23)
+        Me.btnSalvar.TabIndex = 7
+        Me.btnSalvar.Text = "Salvar Dados"
+        Me.btnSalvar.UseVisualStyleBackColor = True
+        '
+        'btnEncerrar
+        '
+        Me.btnEncerrar.Location = New System.Drawing.Point(237, 375)
+        Me.btnEncerrar.Name = "btnEncerrar"
+        Me.btnEncerrar.Size = New System.Drawing.Size(96, 23)
+        Me.btnEncerrar.TabIndex = 9
+        Me.btnEncerrar.Text = "Encerrar"
+        Me.btnEncerrar.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(364, 413)
+        Me.Controls.Add(Me.btnEncerrar)
+        Me.Controls.Add(Me.btnSalvar)
+        Me.Controls.Add(Me.txtNome)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnLocalizar)
+        Me.Controls.Add(Me.picFoto)
+        Me.Controls.Add(Me.lblFoto)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.lblEmail)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.picFoto, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents lblEmail As Label
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents lblFoto As Label
+    Friend WithEvents picFoto As PictureBox
+    Friend WithEvents btnLocalizar As Button
+    Friend WithEvents txtNome As TextBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents btnSalvar As Button
+    Friend WithEvents btnEncerrar As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,74 @@
-﻿Public Class Form1
+﻿Imports MySql.Data.MySqlClient
+Imports System.IO
 
+Public Class Form1
+    Dim nomeArquivoImagem As String
+
+
+    Private Sub btnLocalizar_Click(sender As Object, e As EventArgs) Handles btnLocalizar.Click
+        Try
+            Dim ofd1 As New OpenFileDialog()
+            ofd1.Filter = "Imagens | *.jpg"
+            If ofd1.ShowDialog() = DialogResult.OK Then
+                nomeArquivoImagem = ofd1.FileName
+                picFoto.Image = Image.FromFile(ofd1.FileName)
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnSalvar_Click(sender As Object, e As EventArgs) Handles btnSalvar.Click
+
+        'Dim _conexaoMySQL As String = "Server=localhost;Database=escola;Uid=root;Pwd=123123;"
+        Dim _conexaoMySQL As String = "server=localhost;user id=root;password=123123;database=escola"
+
+        Dim con As MySqlConnection = New MySqlConnection(_conexaoMySQL)
+        Dim cmd As MySqlCommand
+        Dim fs As FileStream
+        Dim br As BinaryReader
+
+        Try
+            If txtNome.Text.Length > 0 And txtEmail.Text.Length > 0 Then
+                Dim NomeArquivoFoto As String = nomeArquivoImagem
+                Dim DadosImagem() As Byte
+
+                fs = New FileStream(NomeArquivoFoto, FileMode.Open, FileAccess.Read)
+                br = New BinaryReader(fs)
+                DadosImagem = br.ReadBytes(CType(fs.Length, Integer))
+                br.Close()
+                fs.Close()
+
+                Dim CmdSql As String = "INSERT INTO alunos(nome, email, imagem) VALUES(@Nome, @Email, @Imagem)"
+                cmd = New MySqlCommand(CmdSql, con)
+
+                cmd.Parameters.Add("@Nome", MySqlDbType.VarChar, 100)
+                cmd.Parameters.Add("@Email", MySqlDbType.VarChar, 150)
+                cmd.Parameters.Add("@Imagem", MySqlDbType.Blob)
+
+                cmd.Parameters("@Nome").Value = txtNome.Text
+                cmd.Parameters("@Email").Value = txtNome.Text
+                cmd.Parameters("@Imagem").Value = DadosImagem
+
+                con.Open()
+
+                Dim linhasAfetadas As Integer = cmd.ExecuteNonQuery()
+                If (linhasAfetadas > 0) Then
+                    MessageBox.Show("A imagem foi salva com sucesso !", "Salvar Imagem", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            Else
+                MessageBox.Show("Dados incompletos!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString())
+        Finally
+            If con.State = ConnectionState.Open Then
+                con.Close()
+            End If
+        End Try
+    End Sub
+
+    Private Sub btnEncerrar_Click(sender As Object, e As EventArgs) Handles btnEncerrar.Click
+        Form1.ActiveForm.Close()
+    End Sub
 End Class
